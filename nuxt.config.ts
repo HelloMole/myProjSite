03eff60fe97b6ca6e7cc36fe0f5ac7e7d847af47
yaowei <648925074@qq.com>
2026-01-12 18:10:56 +0800

    no message

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     preset: 'static',
     publicAssets: [
       {
-        baseURL: '/myProjSite/',
+        baseURL: '/myProjSite',
         dir: 'public'
       }
     ]
